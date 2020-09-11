654440b85ba990a342298bcd8e64beb3206ae314
Lorenzo <lgomez@windhoverlabs.com>
2020-09-11 17:25:52 -0500

    Added baseline docs

--- src/tlm_cmd_merger.py
+++ src/tlm_cmd_merger.py
@@ -241,11 +241,12 @@ def parse_cli() -> argparse.Namespace:
     Parses cli argyments.
     :return: The namespace that has all of the arguments that have been parsed.
     """
-    parser = argparse.ArgumentParser(description='YAML file which contains telemetry and command metadata.')
-    parser.add_argument('-yaml_path', '--yaml_path', metavar='YAML file path', type=str,
-                        help='The file path to the YAML file which contains telemetry and command metadata')
-    parser.add_argument('-sqlite_path', '--sqlite_path', metavar='Sqlite datbase path', type=str,
-                        help='The file path to the sqlite database')
+    parser = argparse.ArgumentParser(description='Takes in paths to yaml file and sqlite database.')
+    parser.add_argument('--yaml_path', type=str,
+                        help='The file path to the YAML file which contains telemetry and command metadata.',
+                        required=True)
+    parser.add_argument('--sqlite_path', type=str,
+                        help='The file path to the sqlite database', required=True)
 
     return parser.parse_args()
 
