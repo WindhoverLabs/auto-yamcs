7de9568f4091aafbb90a109433131825e59bbe52
lorenzo-gomez-windhover <lorenzo-gomez-windhover>
2020-09-22 14:29:18 -0500

    -Updated to work without modules

--- squeeze_airliner.sh
+++ squeeze_airliner.sh
@@ -2,20 +2,20 @@
 #This script runs juicer on airliner
 
 cd juicer
-rm build/new_db.sqlite
+#rm build/new_db.sqlite
 make
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/airliner --module "CFE" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/CFS_LIB.so --module "CFS_LIB" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/CF.so --module "CF" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/CI.so --module "CI" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/CS.so --module "CS" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/DS.so --module "DS" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/FM.so --module "FM" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/HK.so --module "HK" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/HS.so --module "HS" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/LC.so --module "LC" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/MD.so --module "MD" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/MM.so --module "MM" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/SCH.so --module "SCH" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/SC.so --module "SC" --mode SQLITE --output build/new_db.sqlite -v4
-build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/TO.so --module "TO" --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/airliner  --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/CFS_LIB.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/CF.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/CI.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/CS.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/DS.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/FM.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/HK.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/HS.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/LC.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/MD.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/MM.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/SCH.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/SC.so --mode SQLITE --output build/new_db.sqlite -v4
+build/juicer --input /home/vagrant/airliner/build/tutorial/cfs/target/exe/cf/apps/TO.so --mode SQLITE --output build/new_db.sqlite -v4
