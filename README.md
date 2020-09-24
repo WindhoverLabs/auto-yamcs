49659dadaf860f7a3ac22a0bde5222bd30ea7659
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2020-09-24 14:16:54 -0500

    -Added instructions on how to run

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # auto-yamcs
 A collection of tools to auto-generate everything needed to run a ground system.
+
+## How To Use It
+
+1.  You *must* have [airliner](https://github.com/WindhoverLabs/airliner.git)  built on your system in order to use this at the moment. `auto-yamcs` currently only works with the `tutorial/cfs` and `ocpoc` builds.
+
+To create a `tutorial/cfs` build for airliner:
+```
+
+cd airliner
+git checkout add-documentation-to-airliner-tutorial
+make tutorial/cfs
+``` 
+Or an `ocpoc` build:
+
+```
+cd airliner
+git checkout add-documentation-to-airliner-tutorial
+make ocpoc/default 
+```
+
+
+2. Once that builds successfully, you can run the `auto-yamcs` toolchain
+```
+cd auto-yamcs
+python3 squeezer.py --build_dir /home/vagrant/airliner/build/ocpoc/default --output_file newdb --verbosity 4 --yaml_path ./tlm_cmd_merger/src/combined.yml
+
+```
+
+3. Now you can open the database:
+```
+xdg-open juicer/newdb
+
+```
--- squeezer.py
+++ squeezer.py
@@ -1,22 +1,23 @@
 import argparse
 import subprocess
 import os
+from pathlib import Path
 
-file_paths = ['cfs/target/exe/airliner',
-              'cfs/target/exe/cf/apps/CFS_LIB.so',
-              'cfs/target/exe/cf/apps/CF.so',
-              'cfs/target/exe/cf/apps/CI.so',
-              'cfs/target/exe/cf/apps/CS.so',
-              'cfs/target/exe/cf/apps/DS.so',
-              'cfs/target/exe/cf/apps/FM.so',
-              'cfs/target/exe/cf/apps/HK.so',
-              'cfs/target/exe/cf/apps/HS.so',
-              'cfs/target/exe/cf/apps/LC.so',
-              'cfs/target/exe/cf/apps/MD.so',
-              'cfs/target/exe/cf/apps/MM.so',
-              'cfs/target/exe/cf/apps/SCH.so',
-              'cfs/target/exe/cf/apps/SC.so',
-              'cfs/target/exe/cf/apps/TO.so',]
+file_paths = ['target/exe/airliner',
+              'target/exe/cf/apps/CFS_LIB.so',
+              'target/exe/cf/apps/CF.so',
+              'target/exe/cf/apps/CI.so',
+              'target/exe/cf/apps/CS.so',
+              'target/exe/cf/apps/DS.so',
+              'target/exe/cf/apps/FM.so',
+              'target/exe/cf/apps/HK.so',
+              'target/exe/cf/apps/HS.so',
+              'target/exe/cf/apps/LC.so',
+              'target/exe/cf/apps/MD.so',
+              'target/exe/cf/apps/MM.so',
+              'target/exe/cf/apps/SCH.so',
+              'target/exe/cf/apps/SC.so',
+              'target/exe/cf/apps/TO.so',]
 
 
 def parse_cli() -> argparse.Namespace:
@@ -34,23 +35,51 @@ def parse_cli() -> argparse.Namespace:
     parser.add_argument('--verbosity', type=str, default='4', choices=['1','2','3','4'],
                         help='The verbosity with which to run juicer.')
 
+    parser.add_argument('--yaml_path', type=str, default='4',
+                        help='The yaml_path that will be passed to tlm_cmd_merger.py.')
+
+    parser.add_argument('--spacesystem', type=str, default='airliner',
+                        help='The name of the root spacesystem of the xtce file. Note that spacesystem is a synonym '
+                             'for namespace')
+
     return parser.parse_args()
 
 
 def squeeze_files(build_dir: str, output_path: str, mode: str, verbosity: str):
-    subprocess.run(["rm", "build/new_db.sqlite"], cwd='./juicer')
+    subprocess.run(["rm", output_path], cwd='./juicer')
     subprocess.run(["make"], cwd='./juicer')
-    juicer_command = []
 
     for path in file_paths:
         file_path = os.path.join(build_dir, path)
-        juicer_command += 'build/juicer --input'
-        subprocess.run(['build/juicer', '--input',file_path, '--mode', mode, '--output', output_path ,'-v', verbosity], cwd="juicer")
+        print('file path-->', build_dir)
+        my_file = Path(file_path)
+        if my_file.exists():
+            subprocess.run(['build/juicer', '--input', file_path, '--mode', mode, '--output', output_path,'-v', verbosity],
+                            cwd="juicer")
+
+
+def create_venv():
+    subprocess.run(['python3.6', '-m' 'venv', 'venv'])
+    subprocess.run(['./venv/bin/pip', 'install', 'six'])
+    subprocess.run(['./venv/bin/pip', 'install', 'pyyaml'])
+
+
+def merge_files(yaml_path: str, sqlite_path: str):
+    subprocess.run(['../venv/bin/python', '../tlm_cmd_merger/src/tlm_cmd_merger.py', '--yaml_path',
+                    os.path.join('..', yaml_path),
+                    '--sqlite_path', sqlite_path], cwd='juicer')
+
+
+# FIXME: Implement
+def generate_xtce(sqlite_path: str):
+    pass
 
 
 def main():
+    create_venv()
     args = parse_cli()
     squeeze_files(args.build_dir, args.output_file, args.mode, args.verbosity)
+    merge_files(args.yaml_path, args.output_file)
 
 
 if __name__ == '__main__':
