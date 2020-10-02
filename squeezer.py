9775fd630211fbc2aa6ce75b1231d22911c3d112
Lorenzo <lgomez@windhoverlabs.com>
2020-10-02 13:34:19 -0500

    -squeezer uses config file to know which elf files to parse

--- squeezer.py
+++ squeezer.py
@@ -3,57 +3,16 @@ import subprocess
 import os
 import logging
 from pathlib import Path
-
-file_paths = ['target/exe/airliner',
-              'target/exe/cf/apps/CFS_LIB.so',
-              'target/exe/cf/apps/CF.so',
-              'target/exe/cf/apps/CI.so',
-              'target/exe/cf/apps/CS.so',
-              'target/exe/cf/apps/DS.so',
-              'target/exe/cf/apps/FM.so',
-              'target/exe/cf/apps/HK.so',
-              'target/exe/cf/apps/HS.so',
-              'target/exe/cf/apps/LC.so',
-              'target/exe/cf/apps/MD.so',
-              'target/exe/cf/apps/MM.so',
-              'target/exe/cf/apps/SCH.so',
-              'target/exe/cf/apps/SC.so',
-              'target/exe/cf/apps/TO.so']
+import yaml
 
 
-def parse_cli() -> argparse.Namespace:
-    """
-    Parses cli argyments.
-    :return: The namespace that has all of the arguments that have been parsed.
-    """
-    parser = argparse.ArgumentParser(description='Takes in path to sqlite database.')
-    parser.add_argument('--build_dir', type=str, required=True,
-                        help=' The root directory where the elf files are that will be squeezed with juicer')
-    parser.add_argument('--mode', type=str, default='SQLITE', choices=['SQLITE'],
-                        help=' The mode which to run juicer on')
-    parser.add_argument('--output_file', type=str, default='build/newdb', help='The output file juier will write to.')
-
-    parser.add_argument('--verbosity', type=str, default='4', choices=['1', '2', ' 3', '4'],
-                        help='The verbosity with which to run juicer.')
-
-    parser.add_argument('--yaml_path', type=str, required=True,
-                        help='The yaml_path that will be passed to tlm_cmd_merger.py.')
-
-    parser.add_argument('--spacesystem', type=str, default='airliner',
-                        help='The name of the root spacesystem of the xtce file. Note that spacesystem is a synonym '
-                             'for namespace')
-
-    return parser.parse_args()
-
-
-def squeeze_files(build_dir: str, output_path: str, mode: str, verbosity: str):
+def squeeze_files(elf_files: list, output_path: str, mode: str, verbosity: str):
     subprocess.run(['rm', output_path], cwd='./juicer')
     subprocess.run(['make'], cwd='./juicer')
 
-    for path in file_paths:
-        file_path = os.path.join(build_dir, path)
+    for file_path in elf_files:
         my_file = Path(file_path)
-        if my_file.exists():
+        if my_file.exists() and my_file.is_file():
             logging.info(f'Running juicer on {file_path}')
             subprocess.run(
                 ['build/juicer', '--input', file_path, '--mode', mode, '--output', output_path, '-v', verbosity],
@@ -72,17 +31,60 @@ def merge_files(yaml_path: str, sqlite_path: str):
                     '--sqlite_path', sqlite_path], cwd='juicer')
 
 
+def get_elf_files(yaml_dict: dict):
+    elf_files = []
+    elf_files += yaml_dict['core']['elf_files']
+    for module_key in yaml_dict['modules']:
+        for elf in yaml_dict['modules'][module_key]['elf_files']:
+            elf_files.append(elf)
+
+    return elf_files
+
+
 # FIXME: Implement
 def generate_xtce(sqlite_path: str):
     pass
 
 
+def parse_cli() -> argparse.Namespace:
+    """
+    Parses cli argyments.
+    :return: The namespace that has all of the arguments that have been parsed.
+    """
+    parser = argparse.ArgumentParser(description='Takes in path to sqlite database.')
+
+    parser.add_argument('--mode', type=str, default='SQLITE', choices=['SQLITE'],
+                        help=' The mode which to run juicer on')
+    parser.add_argument('--output_file', type=str, default='build/newdb', help='The output file juier will write to.')
+
+    parser.add_argument('--verbosity', type=str, default='4', choices=['1', '2', ' 3', '4'],
+                        help='The verbosity with which to run juicer.')
+
+    parser.add_argument('--yaml_path', type=str, required=True,
+                        help='The yaml_path that will be passed to tlm_cmd_merger.py. This script uses this config file'
+                             ' as well to know which binary files to pass to juicer')
+
+    parser.add_argument('--spacesystem', type=str, default='airliner',
+                        help='The name of the root spacesystem of the xtce file. Note that spacesystem is a synonym '
+                             'for namespace')
+
+    return parser.parse_args()
+
+
+def read_yaml(yaml_file: str) -> dict:
+    yaml_data = yaml.load(open(yaml_file, 'r'),
+                          Loader=yaml.FullLoader)
+    return yaml_data
 
 
 def main():
     create_venv()
     args = parse_cli()
-    squeeze_files(args.build_dir, args.output_file, args.mode, args.verbosity)
+    yaml_dict = read_yaml(args.yaml_path)
+
+    elfs = get_elf_files(yaml_dict)
+
+    squeeze_files(elfs, args.output_file, args.mode, args.verbosity)
     merge_files(args.yaml_path, args.output_file)
 
 
