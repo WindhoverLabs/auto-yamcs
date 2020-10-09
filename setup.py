9b35f2880113bf2009f34fa98e0863b474feb154
Lorenzo <lgomez@windhoverlabs.com>
2020-10-09 13:01:01 -0500

    -Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xtce_generator',
+    version='',
+    packages=['src'],
+    url='',
+    license='',
+    author='Lorenzo Gomez',
+    author_email='',
+    description=''
+)
