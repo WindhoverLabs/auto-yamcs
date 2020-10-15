d6607f25cc4eccf61f032f7b0c370eae19a0b19c
Lorenzo <lgomez@windhoverlabs.com>
2020-10-15 13:45:32 -0500

    -Event ids are written correctly to the database now

--- src/tlm_cmd_merger.py
+++ src/tlm_cmd_merger.py
@@ -250,8 +250,7 @@ def write_event_records(event_data: dict, modules_dict: dict, db_cursor: sqlite3
 
     for module_name in event_data['modules']:
         for event in event_data['modules'][module_name]['events']:
-            event_dict = event_data['modules'][module_name]['events'][event]
-            event_id = event
+            event_id = event['id']
 
             # FIXME: Not sure if we'll read the macro in step of the chain
             # macro = event_dict['macro']
