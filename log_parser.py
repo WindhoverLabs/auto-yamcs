db5c40b3e4710de1d7a66bbd7db1a324b52871a5
Lorenzo <lgomez@windhoverlabs.com>
2020-11-10 17:05:28 -0600

    -Added switch for source of message length

--- log_parser.py
+++ log_parser.py
@@ -6,17 +6,23 @@ application which is part of airliner. You can check it out at [1].
 import argparse
 import sqlite_utils
 import yaml
-from struct import pack, unpack, calcsize
+from struct import unpack, calcsize
 import csv
 from enum import Enum
 from pathlib import Path
-
 import os
+import logging
+
+
+class LengthSource(int, Enum):
+    DATABASE = 1
+    STREAM = 2
+
 
 """
 A mapping between intrinsic types in the database such as int, char, short, etc
 and the format strings specified on [1]. This mapping does NOT include strings. String handling does not rely on
-this mappping.
+this mapping.
 [1]:https://docs.python.org/3/library/struct.html?highlight=struct#format-strings
 """
 symbol_to_struct_format_map = \
@@ -42,6 +48,7 @@ symbol_to_struct_format_map = \
         'bool': '?',
         "boolean": '?',
         'float': 'f',
+        'osalbool': 'B',
         '_padding8': 'c',
         '_padding16': '2c',
         '_padding24': '3c',
@@ -88,15 +95,14 @@ def get_struct_format_string(symbol_id: int, header_size: int, db_handle: sqlite
 
         if type_name not in symbol_to_struct_format_map:
             child_symbol_id = list(db_handle['symbols'].rows_where('name=?', [type_name]))[0]['id']
-            format_string += get_struct_format_string(child_symbol_id, header_size, db_handle, depth+1)
+            format_string += get_struct_format_string(child_symbol_id, header_size, db_handle, depth + 1)
 
         else:
             if field['multiplicity'] == 0:
-                print(f'member_str:{symbol_to_struct_format_map[type_name]}, member_str_size:{calcsize(symbol_to_struct_format_map[type_name])}')
                 format_string += symbol_to_struct_format_map[type_name]
 
             else:
-                #FIXME: If multiplicity is greater than 0 and the type_name is char, we assume this is a string, for now.
+                # FIXME: If multiplicity is greater than 0 and the type_name is char, we assume this is a string, for now.
                 if type_name == 'char':
                     format_string += str(field['multiplicity'])
                     format_string += 's'
@@ -172,6 +178,38 @@ def get_packet_type(stream_id: int):
     return packet_type
 
 
+def message_id_exists(db_handle: sqlite_utils.Database, message_id: int):
+    """
+    Checks if message_id exists either in the telemetry table.
+    :param db_handle:
+    :param message_id:
+    :return:
+    """
+    message_id = list(db_handle['telemetry'].rows_where('message_id=?', [message_id]))
+    out_message_id_exists = False
+    if len(message_id) > 0:
+        out_message_id_exists = True
+
+    return out_message_id_exists
+
+
+def message_id_and_command_code_exists(db_handle: sqlite_utils.Database, message_id: int, command_code: int):
+    """
+    Checks if the record with message_id and command_code exists in the commands table.
+    :param command_code:
+    :param db_handle:
+    :param message_id:
+    :return:
+    """
+    command_record = list(
+        db_handle['commands'].rows_where('message_id=? and command_code=?', [message_id, command_code]))
+    record_exists = False
+    if len(command_record) > 0:
+        record_exists = True
+
+    return record_exists
+
+
 # FIXME:Ignore ccsds headers. Move this function to another function where the get_struct_format_string gets called along with this function
 def get_field_names_from_struct(symbol_id: int, header_size: int, db_handle: sqlite_utils.Database, depth: int = 0):
     field_labels = []
@@ -179,7 +217,7 @@ def get_field_names_from_struct(symbol_id: int, header_size: int, db_handle: sql
 
     # Filter out the ccsds headers
     if depth == 0:
-        filtered_fields = list(filter(lambda record: record['byte_offset'] >= header_size, fields))
+        filtered_fields = list(filter(lambda field_record: field_record['byte_offset'] >= header_size, fields))
 
     else:
         filtered_fields = fields
@@ -194,7 +232,7 @@ def get_field_names_from_struct(symbol_id: int, header_size: int, db_handle: sql
         if type_to_symbol_name[record['type']] in symbol_to_struct_format_map:
             field_labels.append(record['name'])
         else:
-            children_fields = get_field_names_from_struct(record['type'], header_size, db_handle, depth+1)
+            children_fields = get_field_names_from_struct(record['type'], header_size, db_handle, depth + 1)
             field_labels += children_fields
 
     return field_labels
@@ -215,7 +253,8 @@ def write_telemetry_row_to_csv(message_macro: str, message_id: int, symbol_name:
         writer.writerow([message_id, time_in_seconds, symbol_name] + list(filed_values))
 
 
-def write_command_row_to_csv(message_macro: str, command_code: int, message_id: int, symbol_name: str, fields: list, filed_values: tuple):
+def write_command_row_to_csv(message_macro: str, command_code: int, message_id: int, symbol_name: str, fields: list,
+                             filed_values: tuple):
     file_name = message_macro + '_CC_' + str(command_code) + '.csv'
     my_file = Path(file_name)
     does_file_exist = (my_file.exists() and my_file.is_file())
@@ -237,18 +276,26 @@ def get_symbol_name(symbol_id: int, db_handle: sqlite_utils.Database):
 def get_symbol_id_from_message_id(message_id: int, db_handle: sqlite_utils.Database) -> int:
     """
     Returns the symbol id that is mapped to message_id.
+    Be sure to call does_message_id_exist before calling this function.
     :return:
     """
-    symbol_key = list(db_handle['telemetry'].rows_where('message_id=?', [message_id]))[0]['symbol']
+    symbol_record = list(db_handle['telemetry'].rows_where('message_id=?', [message_id]))
+
+    symbol_key = symbol_record[0]['symbol']
+
     return symbol_key
 
 
-def get_symbol_id_from_message_id_and_command_code(message_id: int, command_code: int , db_handle: sqlite_utils.Database) -> int:
+def get_symbol_id_from_message_id_and_command_code(message_id: int, command_code: int,
+                                                   db_handle: sqlite_utils.Database) -> int:
     """
-    Returns the symbol id that is mapped to message_id.
+    Returns the symbol id that is mapped to message_id and command_code in the commands table.
+    Be sure to call does_message_id_exist before calling this function.
     :return:
     """
-    symbol_key = list(db_handle['commands'].rows_where('message_id=? and command_code=?', [message_id, command_code]))[0]['symbol']
+    symbol_key = \
+        list(db_handle['commands'].rows_where('message_id=? and command_code=?', [message_id, command_code]))[0][
+            'symbol']
     return symbol_key
 
 
@@ -258,7 +305,8 @@ def get_telemetry_message_macro(message_id: int, db_handle: sqlite_utils.Databas
 
 
 def get_command_macro(message_id: int, command_code: int, db_handle: sqlite_utils.Database):
-    macro = list(db_handle['commands'].rows_where('message_id=? and command_code=?', [message_id, command_code]))[0]['macro']
+    macro = list(db_handle['commands'].rows_where('message_id=? and command_code=?', [message_id, command_code]))[0][
+        'macro']
     return macro
 
 
@@ -271,11 +319,14 @@ def get_command_code(command_header: int):
     """
     return int("{0:{fill}16b}".format(command_header, fill='0')[8:15], 2)
 
+
 # FIXME: Cleanup code
 
-def parse_file(file_path: str, sqlite_path: str, structures: [str], time_format: TimeFormat):
+def parse_file(file_path: str, sqlite_path: str, structures: [str], time_format: TimeFormat,
+               length_source: LengthSource):
     """
     Parses the file at file_path, extracts all data from it and outputs it into a csv.
+    :param length_source:
     :param file_path:
     :param sqlite_path:
     :param structures:
@@ -296,7 +347,6 @@ def parse_file(file_path: str, sqlite_path: str, structures: [str], time_format:
     current_message_index = 0
     number_of_messages = len(message_data)
 
-    message_ids = set()
     struct_string = None
     struct_size = 0
     symbol_id = None
@@ -316,6 +366,10 @@ def parse_file(file_path: str, sqlite_path: str, structures: [str], time_format:
         stream_id, sequence, length = unpack('!HHH', primary_header)
 
         if get_packet_type(stream_id) == PacketType.TELEMETRY:
+            if message_id_exists(db, stream_id) is False:
+                logging.warning(f'Message id {stream_id} was not found in the database.'
+                                f'Aborting parser.')
+                return
             ccsds_header_length = 6
             if is_secondary_header_present(stream_id):
                 secondary_header = message_data[current_message_index + 6: current_message_index + 12]
@@ -326,8 +380,8 @@ def parse_file(file_path: str, sqlite_path: str, structures: [str], time_format:
             if not (stream_id in telemetry_symbol_map):
                 symbol_id = get_symbol_id_from_message_id(stream_id, db)
                 symbol_name = get_symbol_name(symbol_id, db)
-                struct_string = get_struct_format_string(symbol_id, ccsds_header_length , db)
-                symbol_field_labels = get_field_names_from_struct(symbol_id, ccsds_header_length ,db)
+                struct_string = get_struct_format_string(symbol_id, ccsds_header_length, db)
+                symbol_field_labels = get_field_names_from_struct(symbol_id, ccsds_header_length, db)
                 struct_size = calcsize(struct_string)
                 message_macro = get_telemetry_message_macro(stream_id, db)
 
@@ -340,19 +394,24 @@ def parse_file(file_path: str, sqlite_path: str, structures: [str], time_format:
                                                   'symbol_name': symbol_name,
                                                   'message_macro': message_macro}
                                              })
-            current_message_index += ccsds_header_length
+            # current_message_index += ccsds_header_length
 
-            write_telemetry_row_to_csv(telemetry_symbol_map[stream_id]['message_macro'] ,stream_id,
+            write_telemetry_row_to_csv(telemetry_symbol_map[stream_id]['message_macro'], stream_id,
                                        telemetry_symbol_map[stream_id]['symbol_name'],
                                        telemetry_symbol_map[stream_id]['symbol_field_labels'],
                                        unpack(telemetry_symbol_map[stream_id]['struct_string'],
-                                              message_data[current_message_index:current_message_index +
-                                                                                 telemetry_symbol_map[stream_id][
-                                                                                     'struct_size']]),
+                                              message_data[
+                                              current_message_index + ccsds_header_length:current_message_index + ccsds_header_length +
+                                                                                          telemetry_symbol_map[
+                                                                                              stream_id][
+                                                                                              'struct_size']]),
                                        time_in_seconds)
 
-            current_message_index += telemetry_symbol_map[stream_id]['struct_size']
+            if length_source == LengthSource.STREAM:
+                current_message_index += length + 7
 
+            elif length_source == LengthSource.DATABASE:
+                current_message_index += ccsds_header_length + telemetry_symbol_map[stream_id]['struct_size']
 
         elif get_packet_type(stream_id) == PacketType.COMMAND:
             ccsds_header_length = 6
@@ -361,38 +420,54 @@ def parse_file(file_path: str, sqlite_path: str, structures: [str], time_format:
                 ccsds_header_length = 8
                 command, = unpack('!H', command_secondary_header)
                 command_code = get_command_code(command)
+
+                if message_id_and_command_code_exists(db, stream_id, command_code) is False:
+                    logging.warning(f'Message id {stream_id} with command_code {command_code} was not found in the '
+                                    f'database. '
+                                    f'Aborting parser.')
+                    return
+
                 if not ((stream_id, command_code) in commands_symbol_map):
                     symbol_id = get_symbol_id_from_message_id_and_command_code(stream_id, command_code, db)
                     symbol_name = get_symbol_name(symbol_id, db)
-                    struct_string = get_struct_format_string(symbol_id,  ccsds_header_length, db)
-                    symbol_field_labels = get_field_names_from_struct(symbol_id, ccsds_header_length ,db)
+                    struct_string = get_struct_format_string(symbol_id, ccsds_header_length, db)
+                    symbol_field_labels = get_field_names_from_struct(symbol_id, ccsds_header_length, db)
                     struct_size = calcsize(struct_string)
                     command_macro = get_command_macro(stream_id, command_code, db)
 
                     # NOTE: Please note that symbol and struct are synonyms in this context; they mean the same thing.
                     commands_symbol_map.update({(stream_id, command_code):
-                                                     {'symvol_id': symbol_id,
-                                                      'struct_string': struct_string,
-                                                      'symbol_field_labels': symbol_field_labels,
-                                                      'struct_size': struct_size,
-                                                      'symbol_name': symbol_name,
-                                                      'command_macro': command_macro,
-                                                      'command_code': command_code}
-                                                 })
-                current_message_index += ccsds_header_length
+                                                    {'symvol_id': symbol_id,
+                                                     'struct_string': struct_string,
+                                                     'symbol_field_labels': symbol_field_labels,
+                                                     'struct_size': struct_size,
+                                                     'symbol_name': symbol_name,
+                                                     'command_macro': command_macro,
+                                                     'command_code': command_code}
+                                                })
 
-            write_command_row_to_csv(commands_symbol_map[(stream_id, command_code)]['command_macro'], command_code, stream_id,
-                                       commands_symbol_map[(stream_id, command_code)]['symbol_name'],
-                                       commands_symbol_map[(stream_id, command_code)]['symbol_field_labels'],
-                                       unpack(commands_symbol_map[(stream_id, command_code)]['struct_string'],
-                                              message_data[current_message_index:current_message_index +
-                                                                                 commands_symbol_map[(stream_id, command_code)][
-                                                                                     'struct_size']]))
+                # current_message_index += ccsds_header_length
 
-            current_message_index += commands_symbol_map[(stream_id, command_code)]['struct_size']
+            write_command_row_to_csv(commands_symbol_map[(stream_id, command_code)]['command_macro'], command_code,
+                                     stream_id,
+                                     commands_symbol_map[(stream_id, command_code)]['symbol_name'],
+                                     commands_symbol_map[(stream_id, command_code)]['symbol_field_labels'],
+                                     unpack(commands_symbol_map[(stream_id, command_code)]['struct_string'],
+                                            message_data[
+                                            current_message_index + ccsds_header_length:current_message_index + ccsds_header_length +
+                                                                                        commands_symbol_map[
+                                                                                            (stream_id, command_code)][
+                                                                                            'struct_size']]))
+            if length_source == LengthSource.STREAM:
+                current_message_index += length + 7
 
-
-        message_ids.add(stream_id)
+            elif length_source == LengthSource.DATABASE:
+                current_message_index += ccsds_header_length + commands_symbol_map[(stream_id, command_code)][
+                    'struct_size']
+        else:
+            logging.warning(f"Unknown packet type."
+                            f"Aborting parser.")
+            return
 
 
 str_to_time_enum = \
@@ -405,7 +480,7 @@ str_to_time_enum = \
 
 def parse_cli() -> argparse.Namespace:
     """
-    Parses cli argyments.
+    Parses cli arguments.
     :return: The namespace that has all of the arguments that have been parsed.
     """
     parser = argparse.ArgumentParser(description='Takes in paths to yaml file and sqlite database.')
@@ -424,6 +499,11 @@ def parse_cli() -> argparse.Namespace:
                         choices=['CFE_SB_TIME_32_16_SUBS', 'CFE_SB_TIME_32_32_SUBS', 'CFE_SB_TIME_32_32_M_20'],
                         help='The time format for the ccsds header.')
 
+    parser.add_argument('--message_length_source', type=int, choices=[1, 2], default=2,
+                        help='Which source should the parser use as the source of truth for the length of a message.'
+                             'This length is used as an offset to move to the next message in the stream.'
+                             'Use 1 for database. 2 for the length inside the input file.')
+
     return parser.parse_args()
 
 
@@ -431,8 +511,8 @@ def main():
     args = parse_cli()
 
     yaml_structs = read_yaml(args.structures_yaml)
-    parse_file(args.input_file, args.sqlite_path, get_structure_names(yaml_structs), str_to_time_enum[args.time_format])
-
+    parse_file(args.input_file, args.sqlite_path, get_structure_names(yaml_structs), str_to_time_enum[args.time_format],
+               LengthSource(args.message_length_source))
 
 
 if __name__ == '__main__':
