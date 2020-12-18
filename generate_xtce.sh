96b479da70f5892d5a9b0655ac26177b51771e9d
Mathew Benson <mbenson@windhoverlabs.com>
2020-12-18 10:14:39 -0600

    Added a script to generate the XTCE file that be called from anywhere.

---
+++ generate_xtce.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Read input arguments
+YAML_PATH=$1
+OUTPUT_DB_FILE=$2
+OUTPUT_XTCE_FILE=$3
+
+# Get directory of this script no matter where it is being called from.
+AUTO_YAMCS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Delete any existing files, if there are any.
+if test -f "${OUTPUT_DB_FILE}"; then
+    echo "Cleaning database."; \
+    rm ${OUTPUT_DB_FILE}
+fi
+if test -f "${OUTPUT_XTCE_FILE}"; then
+    echo "Cleaning XTCE."; \
+    rm ${OUTPUT_XTCE_FILE}
+fi
+
+# Set working directory to the auto_yamcs directory.
+cd ${AUTO_YAMCS_DIR}
+
+python3 squeezer.py  --spacesystem cfs --yaml_path ${YAML_PATH} --output_file ${OUTPUT_DB_FILE} --verbosity 1 --xtce_config_yaml ./xtce_generator/src/config.yaml
