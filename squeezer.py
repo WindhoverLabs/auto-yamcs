df95e630b8e0afc75cab450683b58d4ae37efe01
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2020-12-21 12:26:11 -0600

    -Added handlers for inline and singleton modes -TODO:singleton mode is not done yet

--- squeezer.py
+++ squeezer.py
@@ -8,6 +8,7 @@ import tlm_cmd_merger.src.tlm_cmd_merger as tlm_cmd_merger
 import sys
 import remap_symbols
 from enum import Enum
+
 # There does not seem to be a cleaner way of doing this in python when working with git submodules
 sys.path.append(os.path.join(os.getcwd(), 'xtce_generator'))
 
@@ -51,43 +52,71 @@ def run_xtce_generator(sqlite_path: str, xtce_yaml: str, root_spacesystem: str, 
     xtce_generator.generate_xtce(sqlite_path, xtce_yaml, root_spacesystem)
 
 
+def inline_mode_handler(args: argparse.Namespace):
+    yaml_dict = read_yaml(args.yaml_path)
+    set_log_level(args.verbosity)
+
+    elfs = get_elf_files(yaml_dict)
+
+    squeeze_files(elfs, args.output_file, args.juicer_mode, args.verbosity)
+    merge_files(args.yaml_path, args.output_file)
+
+    if args.remap_yaml:
+        remap(args.output_file, args.remap_yaml)
+
+    if args.sql_yaml:
+        run_mod_sql(args.output_file, args.sql_yaml)
+
+    run_xtce_generator(args.output_file, args.xtce_config_yaml, args.spacesystem, args.verbosity)
+
+
+def singleton_mode_handler(args: argparse.Namespace):
+    yaml_dict = read_yaml(args.yaml_path)
+    set_log_level(args.verbosity)
+    print('YAML:', get_remaps(yaml_dict))  #
+    # elfs = get_elf_files(yaml_dict)
+    #
+    # squeeze_files(elfs, args.output_file, args.juicer_mode, args.verbosity)
+    # merge_files(args.yaml_path, args.output_file)
+
+
 def parse_cli() -> argparse.Namespace:
     """
     Parses cli arguments.
     :return: The namespace that has all of the arguments that have been parsed.
     """
-    base_parser = argparse.ArgumentParser(description='Takes in path to sqlite database.', add_help=False)
+    parser = argparse.ArgumentParser(add_help=False)
 
-    base_parser.add_argument('--juicer_mode', type=str, default='SQLITE', choices=['SQLITE'],
-                             help='The mode which to run juicer on')
+    parent_parser = argparse.ArgumentParser(add_help=False)
 
-    base_parser.add_argument('--output_file', type=str, default='newdb.sqlite',
-                             help='The output file juier will write to.', required=True)
+    parent_parser.add_argument('--juicer_mode', type=str, default='SQLITE', choices=['SQLITE'],
+                               help='The mode which to run juicer on')
 
-    base_parser.add_argument('--verbosity', type=str, default='0', choices=['0', '1', '2', '3', '4'],
-                             help='[(0=SILENT), (1=ERRORS), (2=WARNINGS), (3=INFO), (4=DEBUG)]')
+    parent_parser.add_argument('--output_file', type=str, default='newdb.sqlite',
+                               help='The output file juier will write to.', required=True)
 
-    subparsers = base_parser.add_subparsers(
+    parent_parser.add_argument('--verbosity', type=str, default='0', choices=['0', '1', '2', '3', '4'],
+                               help='[(0=SILENT), (1=ERRORS), (2=WARNINGS), (3=INFO), (4=DEBUG)]')
+
+    subparsers = parser.add_subparsers(
         description='Mode to run squeezer.',
         dest='inline | singleton',
         help='Pass a single YAML or invoke each tool separately.'
     )
 
-
     singleton_parser = subparsers.add_parser(
         'singleton',
         help='Run using a single YAML file.',
-        parents=[base_parser]
+        parents=[parent_parser],
     )
+
     inline_parser = subparsers.add_parser(
         'inline',
         help='Run invoking every tool individually. Can provide flexibility in some cases.',
-        parents=[base_parser]
+        parents=[parent_parser]
     )
-    # inline_parser.add_argument('--output_file', type=str, default='newdb.sqlite', help='The output file juier will write to.')
-
-    # inline_parser.add_argument('--verbosity', type=str, default='0', choices=['0', '1', '2', '3', '4'],
-    #                            help='[(0=SILENT), (1=ERRORS), (2=WARNINGS), (3=INFO), (4=DEBUG)]')
+    singleton_parser.set_defaults(func=singleton_mode_handler)
+    inline_parser.set_defaults(func=inline_mode_handler)
 
     inline_parser.add_argument('--yaml_path', type=str, required=True,
                                help='The yaml_path that will be passed to tlm_cmd_merger.py. '
@@ -95,27 +124,26 @@ def parse_cli() -> argparse.Namespace:
                                     'as well to know which binary files to pass to juicer')
 
     inline_parser.add_argument('--xtce_config_yaml', type=str, required=True,
-                        help='The yaml file that will be passed to xtce_generator. xtce_generator will use this'
-                             ' to map base containers for telemetry and commands.')
+                               help='The yaml file that will be passed to xtce_generator. xtce_generator will use this'
+                                    ' to map base containers for telemetry and commands.')
 
     inline_parser.add_argument('--remap_yaml', type=str, default=None,
-                        help='An optional remap configuration file that can be used to remap symbols in the database after'
-                             'it is created.')
+                               help='An optional remap configuration file that can be used to remap symbols in the database after'
+                                    'it is created.')
 
     inline_parser.add_argument('--sql_yaml', type=str, default=None,
-                        help='An optional config file which can be used to insert extra data into the database after juicer'
-                             'is done parsing.')
+                               help='An optional config file which can be used to insert extra data into the database after juicer'
+                                    'is done parsing.')
 
     inline_parser.add_argument('--spacesystem', type=str, default='airliner',
-                        help='The name of the root spacesystem of the xtce file. Note that spacesystem is a synonym '
-                             'for namespace. The name of this spacesystem is also used as a file name in the form of'
-                             '"spacesystem.xml".')
+                               help='The name of the root spacesystem of the xtce file. Note that spacesystem is a '
+                               'synonym for namespace. The name of this spacesystem is also used as a file name in '
+                               'the form of "spacesystem.xml".')
 
     singleton_parser.add_argument('--singleton_yaml_path', type=str, required=True, help='A single YAML file that'
-                                                                                         'has everything auto-yamcs needs')
+                                  'has everything auto-yamcs needs.')
 
-
-    return base_parser.parse_args()
+    return parser.parse_args()
 
 
 def read_yaml(yaml_file: str) -> dict:
@@ -136,6 +164,31 @@ def remap(database_path: str, remap_yaml_path):
 
 def run_mod_sql(database_path: str, yaml_path):
     mod_sql.mod_sql(database_path, yaml_path)
+
+
+def get_remaps(yaml_dict: dict):
+    remaps = {'remaps': dict()}
+
+    # In our airliner setup, we have a special key called "core"
+    if 'core' in yaml_dict:
+        if 'cfe' in yaml_dict['core']:
+            for module in yaml_dict['core']['cfe']:
+                if 'remaps' in yaml_dict['core']['cfe'][module]:
+                    for symbol_remap in yaml_dict['core']['cfe'][module]:
+                        if symbol_remap in remaps['remaps']:
+                            logging.error(f'The {symbol_remap} remap appears twice in the config file.'
+                                          f'Please review the configuration file.')
+                            raise Exception('Duplicate remapping in configuration file.')
+                        remaps['remaps'].update(symbol_remap)
+
+    for module_key in yaml_dict['modules']:
+        if 'remaps' in yaml_dict['modules'][module_key]:
+            for symbol_remap in yaml_dict['modules'][module_key]:
+                if symbol_remap in remaps['remaps']:
+                    logging.error(f'The {symbol_remap} remap appears twice in the config file.'
+                                  f'Please review the configuration file.')
+                    raise Exception('Duplicate remapping in configuration file.')
+                remaps['remaps'].update(symbol_remap)
 
 
 # FIXME: I don't like the fact I'm repeating code here that is also on xtce_generator.py. Will revise.
@@ -162,36 +215,12 @@ def main():
 
     args = parse_cli()
 
-    # FIXME: Not the cleanest way of handling the modes.
-    if len(sys.argv) <= 1:
-        print('Must pass a mode {inline, singleton}')
-        return -1
+    # FIXME: There has to be a cleaner way of doing this.
+    if 'func' in args:
+        args.func(args)
 
-    elif sys.argv != 'inline' or sys.argv != 'singleton':
-        print('Must pass a mode {inline, singleton}')
-        return -1
-
-    mode = sys.argv[1]
-
-    if mode == 'inline':
-        yaml_dict = read_yaml(args.yaml_path)
-        set_log_level(args.verbosity)
-
-        elfs = get_elf_files(yaml_dict)
-
-        squeeze_files(elfs, args.output_file, args.juicer_mode, args.verbosity)
-        merge_files(args.yaml_path, args.output_file)
-
-        if args.remap_yaml:
-            remap(args.output_file, args.remap_yaml)
-
-        if args.sql_yaml:
-            run_mod_sql(args.output_file, args.sql_yaml)
-
-        run_xtce_generator(args.output_file, args.xtce_config_yaml, args.spacesystem, args.verbosity)
-
-    elif mode == 'singleton':
-        yaml_dict = read_yaml(args.singleton_yaml_path)
+    else:
+        print('A mode must be passed:{"inline", "singleton"}')
 
 
 if __name__ == '__main__':
