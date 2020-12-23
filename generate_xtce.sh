5bc36bc3f7a980819f2695a67d3327a4d3896d8e
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2020-12-23 16:39:10 -0600

    -Fixed bugs in generate_xtce script

--- generate_xtce.sh
+++ generate_xtce.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SPACESYSTEM=cfs
+
 # Read input arguments
 YAML_PATH=$1
 OUTPUT_DB_FILE=$2
@@ -11,14 +13,14 @@ AUTO_YAMCS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd 
 # Delete any existing files, if there are any.
 if test -f "${OUTPUT_DB_FILE}"; then
     echo "Cleaning database."; \
-    rm ${OUTPUT_DB_FILE}
+    rm "${OUTPUT_DB_FILE}"
 fi
-if test -f "${OUTPUT_XTCE_DIR}/cfs.xml"; then
+if test -f "${OUTPUT_XTCE_DIR}"/${SPACESYSTEM}.xml; then
     echo "Cleaning XTCE."; \
-    rm ${OUTPUT_XTCE_DIR}/cfs.xml
+    rm "${OUTPUT_XTCE_DIR}"/${SPACESYSTEM}.xml
 fi
 
 # Set working directory to the auto_yamcs directory.
 cd ${AUTO_YAMCS_DIR}
 
-python3 squeezer.py singleton --singleton_yaml_path ${YAML_PATH}  --output_file ${OUTPUT_DB_FILE} --verbosity 1 --xtce_output_dir OUTPUT_XTCE_DIR
+python3 squeezer.py singleton --singleton_yaml_path "${YAML_PATH}"  --output_file "${OUTPUT_DB_FILE}" --verbosity 1 --xtce_output_dir "${OUTPUT_XTCE_DIR}"
