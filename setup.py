f3cf30ffb48189150ac41a2cc905a23e3ca2aa6c
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2022-04-21 12:57:39 -0500

    -Update docs.

--- setup.py
+++ setup.py
@@ -13,13 +13,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='xtce_generator',
-    version='1.0.8',
+    version='1.0.9',
     url='https://github.com/WindhoverLabs/xtce_generator',
     license='3BSD-3-Clause',
     author='Lorenzo Gomez',
     author_email='lgomez@windhoverlabs.com',
     description='A tool to generate xtce files from a sqlite database. ',
     long_decription=readme,
+    long_description_content_type='text/markdown',
     install_requires=requires,
     packages=setuptools.find_namespace_packages(where="src"),
     package_dir={'':'src'},
